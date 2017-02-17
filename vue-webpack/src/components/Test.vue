1c883da828ab5d7c37182b75547c8a3c6306a831
gochant <guochant@gmail.com>
2017-02-17 18:02:12 +0800

    no message

--- vue-webpack/src/components/Test.vue
+++ vue-webpack/src/components/Test.vue
@@ -16,8 +16,13 @@
 </template>
 
 <script>
-  import Hello from 'components/Hello'
-  import Hello2 from 'components/Hello2'
+  import Vue from 'vue'
+  Vue.component('hello', function (resolve) {
+    // 这个特殊的 require 语法告诉 webpack
+    // 自动将编译后的代码分割成不同的块，
+    // 这些块将通过 Ajax 请求自动下载。
+    require(['components/Hello'], resolve)
+  })
 
   export default {
     name: 'test',
@@ -32,8 +37,9 @@
       }
     },
     components: {
-      Hello,
-      Hello2
+//      'hello': resolve => require(['components/Hello2'], resolve)
+//      Hello
+//      Hello2
     },
     methods: {
       addTab () {
--- vue-webpack/src/mock/routes.js
+++ vue-webpack/src/mock/routes.js
@@ -1,8 +1,24 @@
-export defaut {
+export default {
 
-  ['GET /api/users'] (request, next) {
-    let body = 'world!';
-    next(request.respondWith(body, {status: 200}));
-  },
+  'GET /api/users' (request, next) {
+    let list = [{
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1517 弄'
+    }, {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1519 弄'
+    }, {
+      date: '2016-05-03',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1516 弄'
+    }]
+    next(request.respondWith(list, {status: 200}))
+  }
 
-};
+}
